4a09c2de77ec7e395117a9fa9ccb4afe1fee8315
xec <imraneqbit@proton.me>
2025-04-08 13:36:17 +0000

    some minor polishing , even for a play project , this needs work

--- server.c
+++ server.c
@@ -1,11 +1,13 @@
 #include "server.h"
 
 
-/*array holding sockets*/
+/*array holding clients sockets*/
 int clients[MAX_CLIENTS];
-/*array that has the pids of each client*/
+/*array that has the pid of each client*/
 pid_t clients_pid[MAX_CLIENTS];
+/*array that has the thread id of each client*/
 pthread_t threads[MAX_CLIENTS];
+/*the server socket*/
 int socketfd;
 
 int main()
@@ -13,20 +15,29 @@ int main()
   struct sockaddr_in address;
   client_info information;
   int opt = 1;
+  
+  /*the thread id of the thread that lets 
+   * the operator interface with the server*/
   pthread_t interface_thread;
+  
+  /*initializing , each client will increment this*/
   int clientnumber=0 ;
+
   socketfd= socket(AF_INET,SOCK_STREAM,0);
-  pthread_create(&interface_thread,NULL,server_interface,(void*) NULL);
-  for(int i = 0;i<MAX_CLIENTS;i++)
-  {
-    clients[i] = SOCKET_NOT_IN_USE;
-  }
+
   if(socketfd < 0)
   {
     perror("the system said no sockets :(");
     errno = SOCKET_ERROR;
     goto error_handle;
   }
+
+  address.sin_family = AF_INET;
+  address.sin_port = htons(PORT);
+
+  /*making the socket reusable i guess , 
+   * forgot what old me was doing 
+   * comment right away kids*/
   if ( 0 != setsockopt(socketfd, SOL_SOCKET,
              SO_REUSEADDR, &opt,
              sizeof(opt)))
@@ -34,9 +45,11 @@ int main()
     errno = SETSOCK_ERROR;
     goto error_handle;
   }
-  address.sin_family = AF_INET;
-  address.sin_port = htons(PORT);
 
+
+
+
+  /*settin up the address*/
   if (inet_pton(AF_INET, ADDRESS, &address.sin_addr)
     <= 0) 
  {
@@ -44,6 +57,7 @@ int main()
       "\nInvalid address/ Address not supported \n");
   goto error_handle;
 }
+
 if (0 != bind(socketfd,
               (struct sockaddr*)&address,
               sizeof(address)))
@@ -51,29 +65,65 @@ if (0 != bind(socketfd,
   perror("looks like binding a socket is that hard.\n");
   goto error_handle;
 }
-for(int i=0;i<MAX_CLIENTS;i++) {
-  information.client_number = i;
-  if (0 != listen(socketfd,1))
+
+  /*starting the interface of the server 
+   * it is now mainly used for clean exit*/
+
+  pthread_create(&interface_thread,
+                 NULL,
+                 server_interface,
+                 (void*) NULL);
+
+
+
+  /*initializing the clients sockets*/
+  for(int i = 0;i<MAX_CLIENTS;i++)
   {
-    perror("no one ever wants to actually listen :D");
-    goto error_handle;
-  }
-  if( (clients[i] = accept(socketfd,NULL,NULL)) < 0)
-  {
-    perror("no , not accepted , I am sorry :b");
-    goto error_handle;
+    clients[i] = SOCKET_NOT_IN_USE;
   }
 
+
+
+
+  /*main loop*/
+  for(int i=0;i<MAX_CLIENTS;i++) {
+
+    /*each client is known by his number*/
+    information.client_number = i;
+
+    /*waiting for clients*/
+
+    if (0 != listen(socketfd,1))
+    {
+      perror("no ,i am not listening\n");
+      goto error_handle;
+    }
+
+
+    /*establishing connetctions*/
+    if( (clients[i] = accept(socketfd,NULL,NULL)) < 0)
+    {
+      perror("no , not accepted , I am sorry :b");
+      goto error_handle;
+    }
+
+
+
+    /*the client sends his thread id , i know this is not 
+     * secure, relying on valid input is bad */
     if (0 >= read(clients[i],clients_pid+i,sizeof(pid_t)))
     {
       goto error_handle;
     }
     clientnumber =i;
+
     
-  pthread_create(threads+i,NULL,handle_client,(void*) &clientnumber);
-}
+    /*creating a handler thread for the 
+     * client and going on to the next*/
+    pthread_create(threads+i,NULL,handle_client,(void*) &clientnumber);
+  }
 error_handle:
-close(socketfd);
-exit(0);
+  close(socketfd);
+  exit(0);
 }
 
